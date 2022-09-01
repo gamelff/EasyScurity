2810314883d1236d39e9e8ccd1e458682bcc88d6
Gamelff <98972310+gamelff@users.noreply.github.com>
2022-09-01 09:20:08 -0500

    Update secure.js
    made able to put in javascript url

--- secure.js
+++ secure.js
@@ -1,16 +1,16 @@
-alert("you must type something and clicking cancel wont work");
-let user=prompt("Username:");
-let pass=prompt("Password:");
-if (user===null || pass===null || user==="" || pass==="") {
+alert('you must type something and clicking cancel wont work');
+let user=prompt('Username:');
+let pass=prompt('Password:');
+if (user===null || pass===null || user==='' || pass==='') {
   window.location.reload();
 }
 //put the cotrrect user name in the username here
 //put the correct password in password here
-const correct= ["Username Here_Password Here"];
-const user_pass = user+"_"+pass;
+const correct= ['Username Here_Password Here'];
+const user_pass = user+'_'+pass;
 if (correct.includes(user_pass)) {
   alert('Correct');
-  title.innerHTML="Loged In";
+  title.innerHTML='Loged In';
 }
 else {
   alert('incorrect');
