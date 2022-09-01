3daa93c09c4e18779cd20b92bd1b04404231f291
Gamelff <98972310+gamelff@users.noreply.github.com>
2022-09-01 09:16:22 -0500

    Create secure.js

---
+++ secure.js
@@ -0,0 +1,18 @@
+alert("you must type something and clicking cancel wont work");
+let user=prompt("Username:");
+let pass=prompt("Password:");
+if (user===null || pass===null || user==="" || pass==="") {
+  window.location.reload();
+}
+//put the cotrrect user name in the username here
+//put the correct password in password here
+const correct= ["Username Here_Password Here"];
+const user_pass = user+"_"+pass;
+if (correct.includes(user_pass)) {
+  alert('Correct');
+  title.innerHTML="Loged In";
+}
+else {
+  alert('incorrect');
+  window.open('https://www.youtube.com/watch?v=xvFZjo5PgG0','_self');
+}
